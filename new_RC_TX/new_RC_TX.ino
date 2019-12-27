f0098bf4ac91b2be7021a4173a8b975efab44c70
kostyamat <kostyamat@yahoo.com>
2019-12-27 17:47:52 +0100

    Add files via upload

--- new_RC_TX/new_RC_TX.ino
+++ new_RC_TX/new_RC_TX.ino
@@ -1,9 +1,9 @@
-
+#include <avr/sleep.h>
+#include <avr/power.h>
 #include <SPI.h>                      // библиотека для работы с шиной SPI
-//#include "nRF24L01.h"                 // библиотека радиомодуля
 #include "RF24.h"                     // ещё библиотека радиомодуля
 #include "GyverButton.h"              // библиотека для кнопки "Центр", нужна для поддержки мутаций по зажатию этой кнопки
-#include "GyverEncoder.h"             // библиотека для Энкодера, единственная, которая нормально поддерживает энкодер от мышки
+#include "GyverEncoder.h"             // библиотека для Энкодера, единственная которая поддерживает энкодер от мышки
 #include <printf.h>
 #include <EEPROM.h>
 
@@ -47,35 +47,74 @@ uint8_t check_answer = 0;
 uint16_t toneVal;
 byte buzzerEn;
 
+unsigned long timeToSleep;
 
 
+
+void pciSetup(byte pin) {
+  *digitalPinToPCMSK(pin) |= bit (digitalPinToPCMSKbit(pin));  // Разрешаем PCINT для указанного пина
+  PCIFR  |= bit (digitalPinToPCICRbit(pin)); // Очищаем признак запроса прерывания для соответствующей группы пинов
+  PCICR  |= bit (digitalPinToPCICRbit(pin)); // Разрешаем PCINT для соответствующей группы пинов
+}
+
+
+
+ISR (PCINT2_vect) {                          // Обработчик запросов прерывания от пинов D0..D7
+  cli();                                     // мы проснулись, запрещаем прерывания
+}
+
+
+
+void goingToSleep() {
+  Serial.println(F("Sleep..."));
+  radio.powerDown();
+  delay(5);
+  sei();                                    // Разрешаем обработку прерываний
+  sleep_cpu();                              // Переводим МК в спящий режим
+}
 
 
 
 void setup() {
+  ADCSRA &= ~(1 << ADEN);                                                  // Отключаем АЦП, мы им не пользуемся, немного съэкономит энергию
+  set_sleep_mode(SLEEP_MODE_PWR_DOWN);                                     //Устанавливаем интересующий нас режим сна
+  cli();                                                                   // Временно запрещаем обработку прерываний
+
+  MCUCR != (1 << BODS) | (1 << BODSE);                                     // Отключаем детектор пониженного напряжения питания, им мы тоже не пользуемся
+  MCUCR &= ~(1 << BODSE);
+
   Serial.begin(115200);
   printf_begin();
   EEPROM.begin();
+  sleep_enable();
 
   // ----------- Делаем пины входами и включаем внутреннюю подтяжку к плюсу питания, для всех входов кнопок
   pinMode(buttonUp, INPUT_PULLUP);
   pinMode(buttonCentr, INPUT_PULLUP);
   pinMode(buttonDown, INPUT_PULLUP);
 
-  if (EEPROM.read(100) == 127) {
+  // ----------- подвешиваем прерывания по изменению уровня, для вывода из сна, на пины кнопок и энкодера
+  pciSetup(buttonUp);
+  pciSetup(buttonCentr);
+  pciSetup(buttonDown);
+  pciSetup(CLK);
+  pciSetup(DT);
+  pciSetup(SW);
+
+  if (EEPROM.read(100) == 127) {                // проверяем в ЕЕПРОМ будет ли использоваться буззер, если в ЕЕПРОМ значение 127 - буззер активен
     enableBuzzer = true;
   } else enableBuzzer = false;
 
   if (!digitalRead(buttonCentr)) enableBuzzer = !enableBuzzer;                 // если при включении зажата клавиша "Центр" переключаем режим буззера вкл\выкл
-  EEPROM.update(100, (enableBuzzer ? 127 : 222));
+  EEPROM.update(100, (enableBuzzer ? 127 : 222));                              // и записываем состояние буззера в ЕЕПРОМ
 
-  if (!digitalRead(buttonUp) and !digitalRead(buttonDown)) learning  = true;  // зажатие при включении клавиш "Вверх" и "Вниз активирует режим обучения магнитолы
+  if (!digitalRead(buttonUp) and !digitalRead(buttonDown)) learning  = true;   // зажатие при включении клавиш "Вверх" и "Вниз активирует режим обучения магнитолы
 
-  enc1.setType(TYPE1);                    // тип энкодера TYPE1 одношаговый, TYPE2 двухшаговый. Если энкодер работает странно, смените тип
+  enc1.setType(TYPE1);                          // тип энкодера TYPE1 одношаговый, TYPE2 двухшаговый. Если энкодер работает странно, смените тип
 
 
-  pinMode(light, INPUT);                  // вход "подсветка" если есть такой сигнал в руле
-  pinMode(buzzer, OUTPUT);
+  pinMode(light, INPUT);                        // вход "подсветка" если есть такой сигнал в руле
+  pinMode(buzzer, OUTPUT);                      // устанавливаем пин Буззера как выход
 
 
 
@@ -83,42 +122,44 @@ void setup() {
   buttonU.setDebounce(debounce);                // настройка антидребезга (по умолчанию 80 мс)
   buttonU.setTimeout(timeOut);                  // настройка таймаута на удержание (по умолчанию 500 мс)
   buttonU.setClickTimeout(clickTimeOut);        // настройка таймаута между кликами (по умолчанию 300 мс)
-  buttonC.setType(HIGH_PULL);               // HIGH_PULL - кнопка подключена к GND, пин подтянут к VCC (PIN --- КНОПКА --- GND), LOW_PULL  - кнопка подключена к VCC, пин подтянут к GND
-  buttonC.setDirection(NORM_OPEN);          // NORM_OPEN - нормально-разомкнутая кнопка, NORM_CLOSE - нормально-замкнутая кнопка
+  buttonC.setType(HIGH_PULL);                   // HIGH_PULL - кнопка подключена к GND, пин подтянут к VCC (PIN --- КНОПКА --- GND)
+  buttonC.setDirection(NORM_OPEN);              // NORM_OPEN - нормально-разомкнутая кнопка
 
   //----------- Настройки кнопки Centr
-  buttonC.setDebounce(debounce);                  // настройка антидребезга (по умолчанию 80 мс)
+  buttonC.setDebounce(debounce);                // настройка антидребезга (по умолчанию 80 мс)
   buttonC.setTimeout(timeOut);                  // настройка таймаута на удержание (по умолчанию 500 мс)
-  buttonC.setClickTimeout(clickTimeOut);             // настройка таймаута между кликами (по умолчанию 300 мс)
-  buttonC.setType(HIGH_PULL);               // HIGH_PULL - кнопка подключена к GND, пин подтянут к VCC (PIN --- КНОПКА --- GND), LOW_PULL  - кнопка подключена к VCC, пин подтянут к GND
-  buttonC.setDirection(NORM_OPEN);          // NORM_OPEN - нормально-разомкнутая кнопка, NORM_CLOSE - нормально-замкнутая кнопка
+  buttonC.setClickTimeout(clickTimeOut);        // настройка таймаута между кликами (по умолчанию 300 мс)
+  buttonC.setType(HIGH_PULL);                   // HIGH_PULL - кнопка подключена к GND, пин подтянут к VCC (PIN --- КНОПКА --- GND)
+  buttonC.setDirection(NORM_OPEN);              // NORM_OPEN - нормально-разомкнутая кнопка
 
   //----------- Настройки кнопки Down
-  buttonD.setDebounce(debounce);                  // настройка антидребезга (по умолчанию 80 мс)
+  buttonD.setDebounce(debounce);                // настройка антидребезга (по умолчанию 80 мс)
   buttonD.setTimeout(timeOut);                  // настройка таймаута на удержание (по умолчанию 500 мс)
-  buttonD.setClickTimeout(clickTimeOut);             // настройка таймаута между кликами (по умолчанию 300 мс)
-  buttonC.setType(HIGH_PULL);               // HIGH_PULL - кнопка подключена к GND, пин подтянут к VCC (PIN --- КНОПКА --- GND), LOW_PULL  - кнопка подключена к VCC, пин подтянут к GND
-  buttonC.setDirection(NORM_OPEN);          // NORM_OPEN - нормально-разомкнутая кнопка, NORM_CLOSE - нормально-замкнутая кнопка
+  buttonD.setClickTimeout(clickTimeOut);        // настройка таймаута между кликами (по умолчанию 300 мс)
+  buttonC.setType(HIGH_PULL);                   // HIGH_PULL - кнопка подключена к GND, пин подтянут к VCC (PIN --- КНОПКА --- GND)
+  buttonC.setDirection(NORM_OPEN);              // NORM_OPEN - нормально-разомкнутая кнопка
 
 
 
 
-  radio.begin();                          //активировать библиотеку радио-модуля
+  radio.begin();                          // активировать библиотеку радио-модуля
   delay(250);
-  radio.setPALevel(RF24_PA_LOW);          //уровень мощности передатчика. На выбор RF24_PA_MIN, RF24_PA_LOW, RF24_PA_HIGH, RF24_PA_MAX
-  radio.setDataRate(RF24_250KBPS);        //скорость обмена. На выбор RF24_2MBPS, RF24_1MBPS, RF24_250KBPS, должна быть одинакова на приёмнике и передатчике
-  radio.setAutoAck(1);                    //режим подтверждения приёма, 1 вкл 0 выкл
-  radio.setRetries(1, 15);                //(время между попыткой достучаться, число попыток)
-  radio.enableAckPayload();               //разрешить отсылку данных в ответ на входящий сигнал
-  radio.setPayloadSize(32);               //размер пакета, количество байт
-  radio.setChannel(0x2f);                 //выбираем канал
-  radio.openWritingPipe(addresses[0]);    //мы - труба 0, открываем канал для передачи данных
+  radio.setPALevel(RF24_PA_LOW);          // уровень мощности передатчика. На выбор RF24_PA_MIN, RF24_PA_LOW, RF24_PA_HIGH, RF24_PA_MAX
+  radio.setDataRate(RF24_250KBPS);        // скорость обмена. На выбор RF24_2MBPS, RF24_1MBPS, RF24_250KBPS, должна быть одинакова на приёмнике и передатчике
+  radio.setAutoAck(1);                    // режим подтверждения приёма, 1 вкл 0 выкл
+  radio.setRetries(1, 15);                // (время между попыткой достучаться, число попыток)
+  radio.enableAckPayload();               // разрешить отсылку данных в ответ на входящий сигнал
+  radio.setPayloadSize(32);               // размер пакета, количество байт
+  radio.setChannel(0x2f);                 // выбираем канал
+  radio.openWritingPipe(addresses[0]);    // мы - труба 0, открываем канал для передачи данных
   radio.openReadingPipe(1, addresses[1]); // труба 1, резервировано для получения ответной информации с приемника
-  radio.powerUp();                        //начать работу радиомодуля
-  radio.stopListening();                  //не слушаем радиоэфир, мы передатчик
-  radio.printDetails();                   // печатаем системную информацию радиомодуля при старте
-
+  radio.powerUp();                        // начать работу радиомодуля
+  radio.stopListening();                  // не слушаем радиоэфир, мы передатчик
+  //radio.printDetails();                   // печатаем системную информацию радиомодуля при старте
+  goingToSleep();                         // отправляем контроллер спать
 }
+
+
 
 
 
@@ -128,7 +169,7 @@ void loop() {
   uint8_t keyInt = 0;
   enc1.tick();                            // обязательная функция отработки энкодера. Должна постоянно опрашиваться
 
-  // ----------------- обязательные функции отработки кнопок. Кнопки должны постоянно опрашиваться
+  // ----------------- обязательные функции отработки кнопок. Кнопки должны постоянно опрашиваться при каждом проходе цыкла loop
   buttonU.tick();
   buttonC.tick();
   buttonD.tick();
@@ -136,8 +177,11 @@ void loop() {
 
   // --------------- Отрабатываем клики кнопок
   if (buttonU.isClick()) {
-    keyInt = Rutina(1);
-    Serial.println(F("Click on button CENTR"));
+    keyInt = Rutina(1);               /* функция Rutina опрашивает вход подсветки клавиш (когда включены габариты), а также состояние
+                                         передатчика "обучение магнитолы" и добавляет к коду клавиши число 100 и\или 30. Это укажит приемнику на то, что нужно
+                                         подать напряжение на выход "подсветка" на стороне магнитолы, или на то, что передатчик в режиме "обучение". Все то же
+                                         самое и для других кнопок или энкодера.*/
+    Serial.println(F("Click on button UP"));
     done = true;
     toneVal = Tone1;
   }
@@ -151,7 +195,7 @@ void loop() {
 
   if (buttonD.isClick()) {
     keyInt = Rutina(3);
-    Serial.println(F("Click on button CENTR"));
+    Serial.println(F("Click on button DOWN"));
     done = true;
     toneVal = Tone1;
   }
@@ -208,13 +252,13 @@ void loop() {
     toneVal = Tone1;
   }
 
-  if (enc1.isRight()) {                                    // если был поворот вправо encoder
+  if (enc1.isRight()) {                                    // если был поворот энкодера вправо
     done = true;
     keyInt = Rutina(9);
     Serial.println(F("-> Right"));
     toneVal = Tone1;
   }
-  if (enc1.isLeft()) {                                     // если был поворот влево encoder
+  if (enc1.isLeft()) {                                     // если был поворот энкодера влево
     done = true;
     keyInt = Rutina(10);
     Serial.println(F("<- Left"));
@@ -223,12 +267,14 @@ void loop() {
 
 
 
-  if (done) {
-    if (enableBuzzer) tone(buzzer, toneVal, 100);
+  if (done) {                                              // если что-то нажимали done == true, значит будем передавать в эфир
+    radio.powerUp();
+    //delay(5);
+    if (enableBuzzer) tone(buzzer, toneVal, 100);          // если буззер активирован, - бибикнем, подтверждая нажатия\вращение энкодера
     Serial.println(keyInt);
-    if (!radio.write(&keyInt, sizeof(keyInt))) {           // отправить код по радио
+    if (!radio.write(&keyInt, sizeof(keyInt))) {           // отправить код по радио, и если не удалось...
       Serial.println(F("Sending failed"));
-      if (enableBuzzer) {
+      if (enableBuzzer) {                                  // коротко бибикнем три раза
         tone(buzzer, 2500, 20);
         delay(50);
         tone(buzzer, 2250, 20);
@@ -237,7 +283,14 @@ void loop() {
       }
     }
     Serial.println();
-    done = false;
+    done = false;                                          // в эфир отправили, збрасываем состояние флага done
+    timeToSleep = millis();                   // обновляем таймер ухода ко сну, соответственно ждем 1000мс нажатия следующей клавиши или импульс энкодера
+  }
+
+  if (millis() - timeToSleep >= 1500) {                    // после последних действий ничего не происходило более 1 секунды то...
+    goingToSleep();                                        // уходим спать.
+    timeToSleep = millis();                                /* после того как проснулись по прерыванию, попадаем сюда и взводим таймер ко сну. Если ничего
+                                                              не произойдет (мы больше не нажимаем кнопки), МК снова уснет по истичению таймера. */
   }
 }
 
@@ -246,18 +299,18 @@ void loop() {
 
 int Rutina(byte key) {
 
-  if (digitalRead(light) and !flag_Light) {           // читаем состояние подсветки
+  if (digitalRead(light) and !flag_Light) {                // читаем состояние подсветки
     Serial.println(F("Light is ON"));
     flag_Light = true;
     lightOn = 100;
     done = true;
   }
-  if (!digitalRead(light) and flag_Light) {            // отрабатываем отключение подсветки
+  if (!digitalRead(light) and flag_Light) {                // отрабатываем отключение подсветки
     Serial.println(F("Light is ON"));
     flag_Light = false;
     lightOn = 0;
     done = true;
   }
 
-  return key + lightOn + (learning ? 30 : 0);
+  return key + lightOn + (learning ? 30 : 0);              // возвращаем окончательное значение кода команды для передачи в эфир
 }
